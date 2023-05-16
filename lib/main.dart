9afa479106080c8d8c8eaa1364bc630732c15a40
Kirilll Dolmatov <dolmatov.kd@dns-shop.ru>
2023-05-17 08:16:42 +1000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test app for QTIM',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Center(
+        child: Text(
+          'Test app for QTIM',
+          style: TextStyle(decoration: TextDecoration.none),
+        ),
+      ),
+    );
+  }
+}
